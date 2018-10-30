372764ba336e7e914e3f3db4199bafe6660eddaa
dljcurci <dljcurci@gmail.com>
2018-10-30 13:08:49 -0400

    register user

---
+++ evaBACKEND/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using evaBACKEND.Data;
+using evaBACKEND.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace evaBACKEND.Controllers
+{
+    [Route("account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [Route("create")]
+        [HttpPost]
+        public async Task<IActionResult> CreateUserAsync([FromBody] UserModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("User model is invalid");
+            }
+            var user = new AppUser { UserName = model.Username, Email = model.Email };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return BuildToken(model);
+            }
+            return BadRequest($"User with username: {model.Username} already exist.!");
+        }
+
+        private IActionResult BuildToken(UserModel model)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddDays(7);
+
+            var claims = new[]
+{
+                new Claim(JwtRegisteredClaimNames.UniqueName, model.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            JwtSecurityToken token = new JwtSecurityToken(
+               issuer: _configuration["Jwt:Issuer"],
+               audience: _configuration["Jwt:Issuer"],
+               claims: claims,
+               expires: expiration,
+               signingCredentials: creds);
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+
+        }
+    }
+}
